a7674f1c6f8f79d336214cf84fb62d8f42d62078
Elian Richard <elianrichards@gmail.com>
2021-08-08 11:21:37 +0700

    changes about form (still not working)

--- action.php
+++ action.php
@@ -1,6 +1,7 @@
 <?php
 
-if (isset($_POST['submit'])) {
+if (filter_has_var(INPUT_POST, 'submit')) {
+  echo "hello"
   $name = $_POST['name'];
   $email = $_POST['email'];
   $phone = $_POST['phone'];
@@ -8,10 +9,12 @@ if (isset($_POST['submit'])) {
   $message = $_POST['message'];
 
   $subject = 'Email from elianrichard.my.id contact form'
-  $mailTo = 'admin@elianrichard.my.id';
+  $mailTo = 'admin@hrefid.elianrichard.my.id';
   $headers = 'Message from '.$email;
   $txt = 'You have recieved an email from '.$name.' ('.$company.'). Phone: '.$phone.'\n\n'.$message;
 
-  mail($mailTo, $subject, $txt, $headers);
-  header('Location: contact-us?mailsend')
+  // mail($mailTo, $subject, $txt, $headers);
+  header('Location: contact-us.html')
 }
+
+?>
